4d5633d19e3555b43087f7638d90d39de5396386
yvesjesus <yvesj5@hotmail.com>
2016-09-08 15:37:08 -0500

    Update tablero.js

--- tablero.js
+++ tablero.js
@@ -20,8 +20,10 @@ var escena= new THREE.Scene();
 escena.add(tableroMalla);
 
 var camara= new THREE.PerspectiveCamera();
+var centro = new THREE.Vector3( 5, 10, 5 );
+camara.lookAt()
 camara.position.z=1000;
-
+camara.position.x=1000;
 var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
 document.body.appendChild(renderizador.domElement);
