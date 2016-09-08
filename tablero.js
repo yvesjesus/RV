c41fd6cdde9a1786a370b641f22193ef2b486a08
yvesjesus <yvesj5@hotmail.com>
2016-09-08 15:32:39 -0500

    Update tablero.js

--- tablero.js
+++ tablero.js
@@ -1,11 +1,14 @@
 var cuboGris= new THREE.BoxGeometry(10,10,10);
 var cuboMalla= new THREE.Mesh(cuboGris);
 
-cuboMalla.translate(5,5,5);
+cuboMalla.translate(5,0,5);
 
 
 var tablero= new THREE.Geometry();
 
+tablero.merge(cuboMalla.geometry, cuboMalla.matrix);
+
+cuboMalla.translate(5,20,5);
 tablero.merge(cuboMalla.geometry, cuboMalla.matrix);
 
 
@@ -17,7 +20,7 @@ var escena= new THREE.Scene();
 escena.add(tableroMalla);
 
 var camara= new THREE.PerspectiveCamera();
-camara.position.z=100;
+camara.position.z=1000;
 
 var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
