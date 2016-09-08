6a7cb6ea0be69083c7cb547b812e3142ceb1caf4
yvesjesus <yvesj5@hotmail.com>
2016-09-08 15:18:24 -0500

    Create tablero.js

---
+++ tablero.js
@@ -0,0 +1,21 @@
+var cuboGris= new THREE.BoxGeometry(100,100, 100);
+
+var cuboMalla= new THREE.Mesh(troncoForma);
+
+var tablero= new THREE.Geometry();
+
+tablero.merge(cuboMalla.geometry, cuboMalla.matrix);
+var material= new THREE.MeshNormalMaterial();
+
+var tableroMalla= new THREE.Mesh(tablero, material);
+
+var escena= new THREE.Scene();
+escena.add(tableroMalla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
